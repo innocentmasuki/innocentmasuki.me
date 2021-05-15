fa6a42cd9e05b93f25499c037c77e9f680d82695
Innocent Masuki <innocentmasuki99@gmail.com>
2021-05-15 21:15:19 +0300

    added sending capabilities

---
+++ mail.php
@@ -0,0 +1,27 @@
+<?php 
+if(isset($_POST['send_mail'])){
+    $to = "imasuki@innocentmasuki.me"; // this is your Email address
+    if(isset($_POST['guest_mail'])){
+        $from = $_POST['guest_mail'];
+    }
+    if(isset($_POST['guest_name'])){
+        $full_name = $_POST['guest_name'];
+    }
+    if(isset($_POST['guest_message'])){
+        $text = $_POST['guest_message'];
+    }
+  
+    $subject = $_POST['message_sub'];
+
+    
+    
+    $subject = $subject . " ";
+
+    
+    $message = $full_name . " Wrote the following:" . "\n\n" . $text;
+
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    header('Location: index.html');
+?>
