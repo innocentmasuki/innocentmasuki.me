cdf0de5b674f68e35ea32d4149ad4c755b09bf33
Innocent Masuki <innocentmasuki99@gmail.com>
2021-05-15 22:54:48 +0300

    returned to the previous one

--- mail.php
+++ mail.php
@@ -1,23 +1,30 @@
 <?php 
 if(isset($_POST['send_mail'])){
     $to = "innocentmasuki99@gmail.com"; // this is your Email address
-    if(isset($_POST['guest_mail'])){
+   
         $from = $_POST['guest_mail'];
-    }
-    if(isset($_POST['guest_name'])){
+   
+
         $full_name = $_POST['guest_name'];
-    }
-    if(isset($_POST['guest_message'])){
+   
         $text = $_POST['guest_message'];
-    }
+   
     $subject = $_POST['message_sub'];
-    $subject = $subject . " ";
+
 
     
     $message = $full_name . " Wrote the following:" . "\n\n" . $text;
 
 
     $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-    header('Location: index.html');
+
+    if(mail($to,$subject,$message,$headers)){
+            echo "Successful";
+    }
+    else{
+        echo "failed";
+    }
+    
+    // header('Location: index.html');
+}
 ?>
